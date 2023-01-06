385f49b2898aba264156b75d00d0e681bea39a07
jibundeyare <contact@jibundeyare.com>
2023-01-06 12:02:10 +0100

    Ajout d'un nouveau seeder et modification d'un seeder
    Génération de données aléatoire dans le seeder des plats.
    
    Création du seeder des réservations.

--- database/seeders/PlatSeeder.php
+++ database/seeders/PlatSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use Faker;
 use App\Models\Categorie;
 use App\Models\PhotoPlat;
 use App\Models\Plat;
@@ -17,9 +18,14 @@ class PlatSeeder extends Seeder
      */
     public function run()
     {
+        $faker = Faker\Factory::create('fr_FR');
+
         // toutes les catégories
         // Categorie::all() c'est l'équivalent du SQL 'SELECT * FROM Categorie'
         $categories = Categorie::all();
+        // le nombre d'éléments dans la collection
+        $categoriesCount = $categories->count();
+
         // la première catégorie (entrée)
         $categorieEntree = $categories->first();
         // la deuxième catégorie (id 2 plat)
@@ -27,6 +33,9 @@ class PlatSeeder extends Seeder
         $categoriePlat = Categorie::find(2);
         // la troisième catégorie (id 3 dessert)
         $categorieDessert = Categorie::find(3);
+        // les autres catégories restantes
+        $categoriePetitDejeuner = Categorie::find(4);
+        $categorieBoisson = Categorie::find(5);
 
         // toutes les photos
         $photos = PhotoPlat::all();
@@ -75,6 +84,39 @@ class PlatSeeder extends Seeder
             $plat->photo_plat_id = $platData['photo_plat_id'];
             // affectation d'une catégorie
             $plat->categorie_id = $platData['categorie_id'];
+            // sauvegarde dans la BDD
+            $plat->save();
+        }
+
+        for ($i = 0; $i < 100; $i++) {
+            // création d'un nouveau plat
+            $plat = new Plat();
+            // affectation d'un nom
+            $plat->nom = $faker->words(2, true);
+            // affectation d'une description
+            $plat->description = $faker->words(10, true);
+
+            // affectation d'un prix
+            // le prix est aléatoire, compris entre 1 et 50 avec deux chiffres après la virgule
+            $plat->prix = random_int(100, 5000) / 100;
+            // version alternative avec faker
+            // $plat->prix = $faker->randomFloat(2, 1, 50);
+
+            // sélection du statut epinglé / pas épinglé
+            // le statut épinglé est aléatoire, 0 == false, 1 == true
+            $plat->epingle = (bool) random_int(0, 1);
+
+            // affectation d'une photo
+            $plat->photo_plat_id = $photo->id;
+
+            // affectation d'une catégorie
+            // la catégorie est choisie au hasard
+            // un nombre aléatoire est tiré entre 0 et 5-1 (c-à-d 4)
+            $categorieIndex = random_int(0, $categoriesCount - 1);
+            // on utilise le nombre tiré au hasard pour accéder au Nième élément de la collection de catégories
+            $categorie = $categories->get($categorieIndex);
+            $plat->categorie_id = $categorie->id;
+
             // sauvegarde dans la BDD
             $plat->save();
         }
--- database/seeders/ReservationSeeder.php
+++ database/seeders/ReservationSeeder.php
@@ -14,6 +14,41 @@ class ReservationSeeder extends Seeder
      */
     public function run()
     {
-        //
+        // 2 résa avec des données statiques
+        // nom Foo
+        // prénom Foo
+        // jour 06/01/2023
+        // heure 12:00
+        // tel 0612345678
+        // email foo.foo@example.com
+
+        // nom Bar
+        // prénom Bar
+        // jour 13/01/2023
+        // heure 12:00
+        // tel 0634567812
+        // email bar.bar@example.com
+
+        // 48 résa avec des données aléatoires
+
+        // nom
+        $faker->lastName();
+
+        // prénom
+        $faker->firstName();
+
+        // jour
+        $faker->date('Y-m-d');
+
+        // heure
+        $faker->time('H:i');
+
+        // le nombre de personnes
+
+        // tel
+        $faker->phoneNumber();
+
+        // email
+        $faker->safeEmail();
     }
 }

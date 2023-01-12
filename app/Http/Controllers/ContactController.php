dae93fff8d0933294b88a0f6f2eb55df46d6fe10
jibundeyare <contact@jibundeyare.com>
2023-01-12 17:24:57 +0100

    Ajout d'une requête pour récupérer les catégories dans la page contact

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,8 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ContactController extends Controller
 {
-    //
+    public function index()
+    {
+        // SELECT * FROM categorie ORDER BY id ASC
+        $categories = DB::table('categorie')
+            ->orderBy('id', 'asc')
+            ->get()
+        ;
+
+        return view('contact');
+    }
 }

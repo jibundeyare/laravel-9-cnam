957e516d1ec58210472f3fd19d70f88aa067eb71
jibundeyare <contact@jibundeyare.com>
2023-01-20 15:27:56 +0100

    Insallation de Breeze

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,3 +1,9 @@
 import './bootstrap';
 
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
 console.log('Salut Laravel JS');
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\ContactController;
 use App\Http\Controllers\HelloController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MenuController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -32,3 +33,16 @@ Route::get('/reservation', function () {
 Route::get('/mentions-legales', function () {
     return view('mentions-legales');
 })->name('mentions-legales');
+
+// routes de breeze
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';

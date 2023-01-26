f90e2243f2758a1ec42ac864c661a38ec74905b9
jibundeyare <contact@jibundeyare.com>
2023-01-26 10:00:13 +0100

    Désactivation de la page d'inscription

--- routes/auth.php
+++ routes/auth.php
@@ -12,10 +12,11 @@ use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+    // @warning ces routes sont désactivée car elles permettent à n'importe qui de se créer un compte et accéder au back office
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //             ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
